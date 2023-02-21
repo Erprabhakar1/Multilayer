2a656795f821c76448bb36e441ff6f0b23578400
prabhakar tiwari <prabhakartiwari3712@gmail.com>
2023-02-22 01:06:55 +0530

    'first'

---
+++ Controllers/EmployeeController .cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer_BAL;
+using CommanLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiLayer_architecture.Controllers
+{
+    public class EmployeeController_cs : Controller
+    {
+        EmployeeBusiness business = new EmployeeBusiness(); 
+        public IActionResult Index()
+        {
+            var employee = new List<Employee>();
+            try
+            {
+                employee = business.GetEmployee();
+                return View(employee);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+   
+        }
+    }
+}
